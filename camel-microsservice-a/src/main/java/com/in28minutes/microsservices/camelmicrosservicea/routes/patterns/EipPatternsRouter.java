58bf7342ce09e4b4d161e9f4fb012313190a4fac
vcdl1997 <vcdl1997@gmail.com>
2023-08-16 00:56:38 -0300

    feat: finalização do curso de introducao com apache camel

--- camel-microsservice-a/src/main/java/com/in28minutes/microsservices/camelmicrosservicea/routes/patterns/EipPatternsRouter.java
+++ camel-microsservice-a/src/main/java/com/in28minutes/microsservices/camelmicrosservicea/routes/patterns/EipPatternsRouter.java
@@ -1,12 +1,18 @@
 package com.in28minutes.microsservices.camelmicrosservicea.routes.patterns;
 
 import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.camel.AggregationStrategy;
+import org.apache.camel.Body;
 import org.apache.camel.Exchange;
+import org.apache.camel.ExchangeProperties;
+import org.apache.camel.Headers;
 import org.apache.camel.builder.RouteBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +26,8 @@ import com.in28minutes.microsservices.camelmicrosservicea.dto.CurrencyExchangeDt
 
 @Component
 public class EipPatternsRouter extends RouteBuilder{
+	
+	private DynamicRouterBean dynamicRouterBean;
 	
 	@Override
 	public void configure() throws Exception {
@@ -65,19 +73,46 @@ public class EipPatternsRouter extends RouteBuilder{
 		//;
 		
 		
+		
 		// Routing list Pattern
 		/*
 		 * Redirecting a test message to two endpoints at the same time
 		 * */
-		String routingSlip = "direct:endpoint1,direct:endpoint2";
+		//String routingSlip = "direct:endpoint1,direct:endpoint2";
+		//from("timer:routingSlip?period=10000")
+		//.transform().constant("Teste")
+		//.routingSlip(simple(routingSlip))
+		//;
+		//from("direct:endpoint1").to("log:directendpoint1");
+		//from("direct:endpoint2").to("log:directendpoint2");
+		
+		
+		
+		// Dynamic routing list Pattern
+		/*
+		 * Redirecting a test message to three endpoints at the same time based on your logic
+		 * */
+		
+		// Demonstra toda o processo de roteamento que está sendo feita através dos logs
+		//getContext().setTracing(true);
+		
+		// Captura todas as mensagens que não tenham sido processadas e envia para outra fila
+		//errorHandler(deadLetterChannel("activemq:dead-letter-queue"));
 		
 		from("timer:routingSlip?period=10000")
-			.transform().constant("Teste")
-			.routingSlip(simple(routingSlip))
-		;
+		.transform().constant("Teste")
+		.dynamicRouter(method("dynamicRouterBean"));
 		
-		from("direct:endpoint1").to("log:directendpoint1");
+		from("direct:endpoint1")
+		.wireTap("direct:wire-tap") // lets you forward messages to a separate location 
+		.to("log:directendpoint1");
+		
+		from("direct:wire-tap")
+	    .delay(1000).setBody().constant("Tapped")
+	    .to("log:tap");
+		
 		from("direct:endpoint2").to("log:directendpoint2");
+		from("direct:endpoint3").to("log:directendpoint3");
 	}
 }
 
@@ -147,5 +182,31 @@ class ArrayListAggregationStrategy implements AggregationStrategy{
 	         
 	        }
 	        return currentExchange;
+    }
+}
+
+@Component
+class DynamicRouterBean {
+	
+	Logger logger = LoggerFactory.getLogger(DynamicRouterBean.class);
+	
+	int invocations = 0;
+	
+	public String decideTheNextEndpoint(
+		@ExchangeProperties Map<String, String> properties,
+		@Headers Map<String, String> headers,
+		@Body String body
+	) {
+		logger.info("{} {} {}", properties, headers, body);
+		
+		++invocations;
+		
+		if(invocations % 3 == 0) {
+			return "direct:endpoint1";
+		}else if(invocations % 3 == 1) {
+			return "direct:endpoint2,direct:endpoint3";
+		}
+			
+		return null;
     }
 }
